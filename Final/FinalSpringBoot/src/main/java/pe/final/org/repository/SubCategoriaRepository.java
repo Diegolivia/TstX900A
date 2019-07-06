32a43d24d9fc55f6be535509551d5d80da9bc6a2
Diego Livia Ortiz <diegolivia@hotmail.com>
2019-07-06 07:26:18 -0500

    fin aos

---
+++ Final/FinalSpringBoot/src/main/java/pe/final/org/repository/SubCategoriaRepository.java
@@ -0,0 +1,14 @@
+package pe.roffus.org.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pe.roffus.org.model.SubCategoria;
+
+import java.util.List;
+
+public interface SubCategoriaRepository extends JpaRepository<SubCategoria,Integer> {
+
+    @Query(value="select s.cod_sub_categoria, s.nombre_sub_categoria, s.cod_categoria from sub_categoria s where s.cod_categoria=?1",nativeQuery = true)
+    List<SubCategoria> findSubCategoriaByCategoria(int id);
+}
---
+++ Final/FinalSpringBoot/src/main/java/pe/final/org/service/SubCategoriaService.java
@@ -0,0 +1,9 @@
+package pe.roffus.org.service;
+
+import  pe.roffus.org.model.SubCategoria;
+
+import java.util.List;
+
+public interface SubCategoriaService extends CRUDService<SubCategoria> {
+    public List<SubCategoria> findSubCategoriaByCategoria(int id);
+}
